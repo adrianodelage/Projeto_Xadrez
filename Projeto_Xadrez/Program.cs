a45d7e0274e0428776614d5f45109a651b76b5d5
Adriano Cardoso <adriano.cardoso@delage.com.br>
2018-10-24 08:58:39 -0300

    02 - Class Tabuleiro

--- Projeto_Xadrez/Program.cs
+++ Projeto_Xadrez/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tabuleiro;
+
 namespace Projeto_Xadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            //aaa
+
+            Posicao P = new Posicao(3, 4);
+
+            //Console.WriteLine("Posição: " + P.linha + " Coluna: " + P.coluna);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+
+
         }
     }
 }
---
+++ Projeto_Xadrez/Tabuleiro/Posicao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuleiro
+{
+    class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        // Construtor
+
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        // Método To string converte a saida
+        public override string ToString()
+        {
+            return linha + ","+  coluna;
+        }
+
+
+    }
+}
