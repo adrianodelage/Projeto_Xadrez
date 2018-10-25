c6c64ac4dee76b4db8e17141cca1b463678d29ec
Adriano Cardoso <adriano.cardoso@delage.com.br>
2018-10-25 08:54:42 -0300

    03 - Classes do Tabuleiro

---
+++ Projeto_Xadrez/Tela.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Projeto_Xadrez
+{
+    class Tela
+    {
+
+        //metod VOID não retorna nada:
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+
+        }
+
+
+    }
+}
