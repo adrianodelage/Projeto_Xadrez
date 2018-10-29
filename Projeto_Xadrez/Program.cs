74fac1af62868e08b9af1a016de8a9e2896dea48
Adriano Cardoso <adriano.cardoso@delage.com.br>
2018-10-29 09:01:38 -0300

    Criando movimentação

--- Projeto_Xadrez/Program.cs
+++ Projeto_Xadrez/Program.cs
@@ -18,9 +18,25 @@ namespace Projeto_Xadrez
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
+                while (!partida.terminada) {
 
-                // Console.WriteLine("");
-                Tela.imprimirTabuleiro(partida.tab);
+                    Console.Clear();
+                    // Console.WriteLine("");
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine("");
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+
+                }
+
+
+
             }
             catch (TabuleiroException e)
             {
